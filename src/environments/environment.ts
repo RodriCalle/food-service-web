e03272a8cb70b929a6e66aca6cc8963728081aad
Rodri Calle <rodrigocallegaldos@gmail.com>
2025-08-08 18:23:27 -0500

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://api.example.com',
+  production: true,
+  apiUrl:
+    'https://food-service-system-gfa8f0eudraeabhx.westus2-01.azurewebsites.net',
 };
